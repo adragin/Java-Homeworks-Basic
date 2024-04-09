6a54a4e0fb3dace74efe6a7f081eb393cc4547cc
Aleksandr <46357704+adragin@users.noreply.github.com>
2024-04-09 14:58:28 +0200

    HW-2 v2
    изменение вывода трехзначного числа

--- Homework_02.java
+++ Homework_02.java
@@ -15,11 +15,10 @@ public class Homework_02 {
         System.out.println("c / 0 = " + (c / 0));
         System.out.println("");
 
-        int num = 984;
-        System.out.println("In number " + num + " :");
-        System.out.println("  hundrets " + (num / 100));
-        System.out.println("  decimals " + (num /10 - ((num / 100) * 10)));
-        System.out.println("  units    " + num % 10);
-
+        int num = 345;
+        System.out.println("Число " + num + " : "
+                + (num / 100) + ", "
+                + (num /10 - ((num / 100) * 10)) + ", "
+                + num % 10);
     }
 }
