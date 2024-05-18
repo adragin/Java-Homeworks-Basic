49d44b1739ab8a134e1586984a7aea360500a3f8
Aleksandr <46357704+adragin@users.noreply.github.com>
2024-05-18 21:52:00 +0200

    Homework_14052024

--- Homework_14052024.java
+++ Homework_14052024.java
@@ -5,34 +5,53 @@ package com.telran.homeworks;
 //  {"model", "V-60", "country", "Germany", "city", "Berlin", "year", null, "active", true}
 //  Пример: {"id", 1, "country", null, "city", "Helsinki", "year", null}
 //  Результат: SELECT * FROM users WHERE id = '1'  AND city = 'Helsilnki';
-//
-//  2. Метод isValidAccountNumber(String accountNumber) покрыть тестами
 
 public class Homework_14052024 {
     public static void main(String[] args) {
-//        String id = "1";
-//        String model = "V-60";
-//        String country = "Germany";
-//        String city = "Berlin";
-//        String year = null;
-//        String active = "true";
 
+        String inputData = "{\"model\", \"V-60\", \"country\", \"Germany\", \"city\", \"Berlin\", \"year\", null, \"active\", true}";
         String tableName = "cars";
-        String[] parameterName = {"id", "model", "country" , "city"  , "year", "active"};
-        String[] parameter     = {"1",  "v-60",  "Germany" , "Berlin", null  , "true"};
-//        String[] parameter     = {null,  "v-60",  "Germany" , "Berlin", null  , "true"};
-//        String[] parameter     = {null,  "v-60",  "Germany" , "Berlin", null  , null};
-//        String[] parameter     = {null,  "X-5",  null, null, null  , "true"};
+        int numberOfParameters = getNumbersOfParameters(inputData);
+        String[] parameterName = new String[numberOfParameters];
+        String[] parameter = new String[numberOfParameters];
 
-        StringBuilder sb = new StringBuilder("SELECT * FROM " + tableName + " WHERE");
-        for (int i = 0; i < parameter.length; i++) {
-            if (sb.substring(sb.length() - 5).equals("WHERE")){
-                sb.append(parameter[i] != null ? " " + parameterName[i] + "='" + parameter[i] + "'" : "");
-            } else {
-                sb.append(parameter[i] != null ? " AND " + parameterName[i] + "='" + parameter[i] + "'" : "");
+        splitInputToArrays(inputData, parameterName, parameter);
+        String request = getSelect(tableName, parameterName, parameter);
+        System.out.println(request);
+    }
+
+    public static int getNumbersOfParameters(String str) {
+        int numberOfParameters = 0;
+        int qouteCounter = 0;
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) == '\"') {
+                qouteCounter++;
+            }
+            if (qouteCounter % 2 == 0 && str.charAt(i) == ',') {         // если кавычка закрыта, то запятую считаем
+                numberOfParameters++;
             }
         }
+        return (numberOfParameters + 1) / 2;
+    }
 
-        System.out.println(sb);
+    public static void splitInputToArrays(String str, String[] parNames, String[] params) {
+        String newStr = str.replace("{", "").replace("}", "").replace(", ", ",").replace("\"", "");
+        String[] inputs = newStr.split(",");
+        for (int i = 0; i < inputs.length; i += 2) {
+            parNames[i / 2] = inputs[i];
+            params[i / 2] = inputs[i + 1].equals("null") ? null : inputs[i + 1];
+        }
+    }
+
+    public static String getSelect(String tableName, String[] parameterNames, String[] parameters) {
+        StringBuilder sb = new StringBuilder("SELECT * FROM " + tableName + " WHERE");
+        for (int i = 0; i < parameters.length; i++) {
+            if (sb.substring(sb.length() - 5).equals("WHERE")) {
+                sb.append(parameters[i] != null ? " " + parameterNames[i] + "='" + parameters[i] + "'" : "");
+            } else {
+                sb.append(parameters[i] != null ? " AND " + parameterNames[i] + "='" + parameters[i] + "'" : "");
+            }
+        }
+        return sb.toString();
     }
 }
